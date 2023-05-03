ef59fd59a528a1d234d3681b6097f2bc4180d0b8
astechedu <astech.utube@gmail.com>
2023-05-03 10:45:59 +0530

    Core php testing API

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+// https://localhost/index.php/{MODULE_NAME}/{METHOD_NAME}?limit={LIMIT_VALUE}
+//http://localhost/index.php/user/list?limit=20
+
+
+require __DIR__ . "/inc/bootstrap.php";
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = explode( '/', $uri );
+if ((isset($uri[2]) && $uri[2] != 'user') || !isset($uri[3])) {
+    header("HTTP/1.1 404 Not Found");
+    exit();
+}
+require PROJECT_ROOT_PATH . "/Controller/Api/UserController.php";
+$objFeedController = new UserController();
+$strMethodName = $uri[3] . 'Action';
+$objFeedController->{$strMethodName}();
+
+?>
