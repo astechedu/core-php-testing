e0716259426c5c3621f282f7e8563517dace61c6
astechedu <astech.utube@gmail.com>
2023-05-18 21:26:20 +0530

    phpunint 10 test for core php

---
+++ app/Maths.php
@@ -0,0 +1,34 @@
+<?php 
+namespace App;
+
+class Maths {
+	
+   public function add($num1,$num2) {
+      
+      return $num1 + $num2;
+
+   }
+
+   public function sub($num1,$num2) {
+     
+      return $num1 - $num2;     
+
+   }
+
+   public function muliply($num1,$num2) {     
+     
+      return $num1 * $num2;
+
+   }  
+ 
+   public function devide($num1,$num2) {     
+     
+      return $num1 / $num2;     
+
+   }     
+   
+}
+
+
+
+?>
--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -2,14 +2,18 @@
 
 use PHPUnit\Framework\TestCase;
 
+//Method 3
+use App\Maths;
 
 class CalculatorTest extends TestCase {
 	
+  //Method 1
    public function testAddCanNumbers() {
       
      $this->assertSame(2,(1+1));
    }
 
+/*
    public function testSbtractCanNumbers() {
      
      $this->assertSame(0,(1-1));
@@ -26,9 +30,34 @@ class CalculatorTest extends TestCase {
      
    }  
 
+*/
 
-   
- 
+
+//Method 2
+//Creating Object from Maths.php file in app folder
+
+  public function testAdd() {
+
+      $maths = new Maths();
+
+      $add = $maths->add(2,2);
+
+      $this->assertEquals(4, $add);
+
+  }
+  
+     
+  public function testSub() {
+
+      $maths = new Maths();
+
+      $sub = $maths->sub(2,2);
+
+      $this->assertEquals(0, $sub);
+
+  } 
+
+  
    
 }
 
