f2e7d606eb1e3fc29482f9d9f503bd8f0c7df6f9
astechedu <astech.utube@gmail.com>
2023-05-24 14:09:53 +0530

    core php shopping cart testing

--- addToCart.logics.php
+++ addToCart.logics.php
@@ -6,6 +6,60 @@ Key: cart logic 1, cart logic 2, tables,
 //---------------------------------------------------------
 
 
+
+//---------------------------------------------------------------------------
+//Shopping Cart My Code:
+//It is working.
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){      
+   
+  if(isset($_POST['submit'])){
+
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $price = $_POST['price'];
+    $qty = $_POST['quantity'];
+    $code = $_POST['code'];
+
+  $cartArray = array(   
+    $code=>array(       
+    'id' => $id,
+    'name' => $name,
+    'price' => $price,
+    'quantity' => (int)$qty,
+    'code' => $code      
+    )   
+  );
+
+  if($_SESSION['shopping_cart']) {              
+          //If again same product hit with same id, qty++
+          //echo "<pre>"; print_r($_SESSION['shopping_cart'][$id]);
+         if(!in_array($cartArray[$code]['code'], array_keys($_SESSION['shopping_cart']))){        
+
+            //echo "Found";
+         $_SESSION['shopping_cart'] = array_merge($_SESSION['shopping_cart'],$cartArray);
+         header('location: /');
+
+        }else{      
+
+            //echo 'Not Found';}
+          $_SESSION['shopping_cart'][$code]['quantity'] += $cartArray[$code]['quantity'];
+            header('location: /');          
+
+  }else{
+      //If product hit first time by user
+        $_SESSION['shopping_cart'] = $cartArray;
+        header('location: /');
+        }           
+    }
+}
+
+
+
+//---------------------------------------------------------------------------
+
+
+
 1. Cart Logic 1:
 
 if(isset($_POST["add_to_cart"])){
--- partials/content.php
+++ partials/content.php
@@ -2,16 +2,7 @@
   
 session_start();
 
- //echo "<pre>"; print_r($_SESSION['shopping_cart']);
-//session_destroy($_SESSION['shopping_cart'][counter]);
-//session_destroy($_SESSION['shopping_cart'][0]);
-
-//session_destroy($_SESSION['shopping_cart']);
-
-   //include 'dbcon.php';
-
-    //Object create_function(args, code)
-    $db_conn = new dbcon();   
+$db_conn = new dbcon();   
 
   ?>
 
@@ -59,19 +50,6 @@ session_start();
   </div>
 
 </div>
-    <!--
-    <div class="col-md-4">   
-       <form id="registerform" action="" method="post">
-          <h1>Register</h1>
-          <input type="number" name="id" value="" placeholder="ID" class="form-control">
-          <input type="text" name="name" value="" placeholder="Name" class="form-control">
-          <input type="number" name="salary" value="" placeholder="Salary" class="form-control">
-          <input type="text" name="city" value="" placeholder="City" class="form-control">
-          <input type="submit" name="submit" value="Submit" class="form-control btn btn-info ">
-       </form>
-    </div>
-  </div>
-  -->
 
 <!-- User Register Form -->
 <?php //include 'forms/user_register.php' ;?>
@@ -83,7 +61,6 @@ session_start();
 
 <!-- Footer -->
 <?php include 'footer.php' ;?>
-
 
 
 <style type="text/css">
