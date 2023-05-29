717ecbbe0a9384fd0c03e7926545beee20295a9d
astechedu <astech.utube@gmail.com>
2023-05-30 00:27:22 +0530

    core php car rental testing

--- check_availability.php
+++ check_availability.php
@@ -9,20 +9,21 @@ if(!empty($_POST["emailid"])) {
 	}
 	else {
 		$sql ="SELECT EmailId FROM tblusers WHERE EmailId=:email";
-$query= $dbh -> prepare($sql);
-$query-> bindParam(':email', $email, PDO::PARAM_STR);
-$query-> execute();
-$results = $query -> fetchAll(PDO::FETCH_OBJ);
-$cnt=1;
-if($query -> rowCount() > 0)
-{
-echo "<span style='color:red'> Email already exists .</span>";
- echo "<script>$('#submit').prop('disabled',true);</script>";
-} else{
-	
-	echo "<span style='color:green'> Email available for Registration .</span>";
- echo "<script>$('#submit').prop('disabled',false);</script>";
-}
+		$query= $dbh -> prepare($sql);
+		$query-> bindParam(':email', $email, PDO::PARAM_STR);
+		$query-> execute();
+		$results = $query -> fetchAll(PDO::FETCH_OBJ);
+		$cnt=1;
+		
+	if($query -> rowCount() > 0)
+	{
+		 echo "<span style='color:red'> Email already exists .</span>";
+		 echo "<script>$('#submit').prop('disabled',true);</script>";
+	} else{
+		
+		echo "<span style='color:green'> Email available for Registration .</span>";
+	 	echo "<script>$('#submit').prop('disabled',false);</script>";
+	}
 }
 }
 
--- login.php
+++ login.php
@@ -1,13 +1,13 @@
 <?php 
 
-?>
-
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+?>bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <!------ Include the above in your HEAD tag ---------->
 
 <div class="login-reg-panel">
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="
 		<div class="login-info-box">
 			<h2>Have an account?</h2>
 			<p>Lorem ipsum dolor sit amet</p>
