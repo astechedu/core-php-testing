af5cf76b42318409cb9b4a3d3b5e1b4b8b126921
astechedu <astech.utube@gmail.com>
2023-06-04 14:21:23 +0530

    core php admin testing

--- partials/navbar.php
+++ partials/navbar.php
@@ -2,7 +2,7 @@
 //session_start();
 //
 ?>
-<nav class="navbar navbar-expand-lg bg-primary">
+<nav class="navbar navbar-expand-lg bg-primary fixed-top">
   <div class="container-fluid">
 
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
