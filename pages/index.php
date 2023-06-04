167413efe766e16639561b762a4690b8fb00bc34
astechedu <astech.utube@gmail.com>
2023-06-04 12:26:36 +0530

    core php pagination testing

---
+++ pages/index.php
@@ -0,0 +1,87 @@
+<?php
+
+include "databases/db.php";
+
+if ($_GET["page"]) {
+    $page = $_GET["page"];
+} else {
+    $page = 1;
+}
+
+$total_records = count($db->select($pdo));
+$limit = 3;
+$total_page = ceil($total_records / $limit);
+$offset = ($page - 1) * $limit;
+
+$pagi = $db->pagination($pdo, $offset, $limit);
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Home</title>
+   </head>
+   <body>
+      <div class="container" data-aos="fade-up" data-aos-duration="3000">
+         <div class="row">
+            <div class="col-lg-12">
+               <table class="table">
+                  <thead>
+                     <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Salary</th>
+                        <th scope="col">City</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Created</th>
+                        <th scope="col">Updated</th>
+                     </tr>
+                  </thead>
+                  <tbody>
+                     <?php foreach($pagi as $user) { ?>
+                     <tr>
+                        <td><?= $user['id'] ?></td>
+                        <td><?= $user['name'] ?></td>
+                        <td><?= $user['salary'] ?></td>
+                        <td><?= $user['city'] ?></td>
+                        <td><?= $user['email'] ?></td>
+                        <td><?= $user['created'] ?></td>
+                        <td><?= $user['updated'] ?></td>
+                        <td scope="col">
+                           <a href="#" class="btn btn-info">Edit</a>
+                           <a href="#" class="btn btn-danger">Remove</a>
+                           <a href="#" class="btn btn-success">View</a>
+                        </td>
+                     </tr>
+                     <?php } ?>        
+                  </tbody>
+               </table>
+            </div>
+         </div>
+      </div>
+    
+
+<nav aria-label="Page navigation example">
+	<ul class="pagination">		
+		<?php if ($page > 1) { ?>
+		<li class="page-item"><a class="page-link" href="?page=<?= ($page - 1) ?>">Previous</a></li>
+		<?php } ?>
+<?php for ($i = 0; $i < $total_page; $i++) {
+    echo '		  	    
+	    <li class="page-item"><a class="page-link" href="?page=' .
+        $i .
+        '">' .
+        $i .
+        '</a></li>	    	  
+	   ';
+} ?>      <?php if ($total_page > $page) { ?>
+        <li class="page-item"><a class="page-link" href="?page=<?= ($page + 1) ?>">Next</a></li>
+        <?php } ?>
+	</ul>	
+</nav>
+
+</body>
+</html>
+
---
+++ partials/navbar.php
@@ -0,0 +1,75 @@
+<?php
+//session_start();
+//
+?>
+<nav class="navbar navbar-expand-lg bg-primary">
+  <div class="container-fluid">
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <a class="navbar-brand" href="#">PHP Auth</a>
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/pages/home.php">Home</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">about</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">contact</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="/auth/register.php">Register</a>
+        </li>
+
+        <li class="nav-item">
+
+        <?php if (!isset($_SESSION["logged_in"])) { ?>          
+          <a class="nav-link" href="index.php">login</a>
+         <?php } else { ?>
+          <a class="nav-link" href="/auth/logout.php">logout</a>
+         <?php } ?>
+        </li>      
+          
+        
+        
+        
+        <?php if (
+            isset($_SESSION["logged_in"]) &&
+            $_SESSION["logged_in"] == true
+        ) { ?>
+        <li class="nav-item">
+          <a class="nav-link">Profile:
+               <?php if (
+                   isset($_SESSION["logged_in"]) &&
+                   $_SESSION["logged_in"] == true
+               ) {
+                   echo "<strong>" . $_SESSION["username"] . "</strong>";
+               } ?>
+          </a>
+        </li>
+       <?php } ?>
+
+      </ul>
+      </div>
+
+      <!--
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+      -->
+
+    </div>
+  </div>
+</nav>
