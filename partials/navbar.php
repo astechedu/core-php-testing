8143e8ffb55d42883eb66efcd2d51f2241625ea3
astechedu <astech.utube@gmail.com>
2023-06-05 18:12:25 +0530

    core php admin testing

--- partials/navbar.php
+++ partials/navbar.php
@@ -10,7 +10,7 @@
     </button>
 
     <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <a class="navbar-brand" href="#">PHP Auth</a>
+      <a class="navbar-brand" href="#">PHP Admin</a>
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="/pages/home.php">Home</a>
