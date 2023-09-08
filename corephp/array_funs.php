9fd0cf27d42769d179883a4f5331faa78a548681
astechedu <astech.utube@gmail.com>
2023-09-08 23:18:21 +0530

    php array funs

--- corephp/array_funs.php
+++ corephp/array_funs.php
@@ -6,7 +6,7 @@
 //array_column(),array_key(),array_key_exist(),array_change_key_case(),array_diff(),array_diff_uassoc(),array_diff_assoc(),array_diff_keys(),array_diff_ukey(),array_fill(),array_fill_keys(),array_filter(),array_flip(),array_intersect_assoc(),array_intersect_key(),array_intersect(),array_key_exists(),array_keys(),array_key_recursive(),array_multisort(),array_pad(),array_product(),array_rand(),array_reduce(),array_replace_recursive(),array_replace(),array_reverse(),array_search(),array_slice(),array_splice(),array_sum(),array_udiff_assoc(),array_udiff(),array_intersect_assoc(),array_intersect_uassoc(),array_uintersect(),array_walk(),array_walk_recursive(),compact(),count(),current(),end(),extract(),in_array(),is_array(),key(),next(),ksort(),rsort(),list(),natsort(),natcasesort(),pos(),prev(),range(),reset(),shuffle(),sizeof(),sort(),uasort(),uksort(),usort(),each()
 
 
-//----> array Function <----
+//----> array Functions <----
 
 //$n1 = array(1,2,3,4,5);
 //$n2 = array(6,7,8,9,10);
@@ -18,6 +18,28 @@
 //$as1 = array('e' => 'ramput', 'f' => 'kanput', 'g' => 'noida', 'h' => 'amarnaath');
 //$emp1 = array('name' => 'amit', 'age' => 34, 'city' => 'dhampur');
 
+$users = array("amit","rohit","zubin");
+
+
+
+
+//Today:
+
+//array_push()
+/*
+echo "<pre>";print_r($users);
+echo "No of elements: ".array_push($users,"rekha");
+echo "<pre>";print_r($users);
+*/
+//array_pop()
+
+
+//array_shift()
+echo "<pre>";print_r($users);
+echo "Added Element: ".array_unshift($users,"sonu","kumar");
+echo "<pre>";print_r($users);
+
+//array_unshift()
 
 
 
